08682e76fe8e9156c0e5a1a383ecd9544b5e1f02
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-26 19:44:26 -0600

    commit tree 00e2d435cb7c9b4ff1adf0aaeb7b9fbf3ff57b2a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById('main').remove()
+let newHeader = document.createElement("H1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Connor is the champion"
